eb12c67f71eeeb178de9259520fa9729de355c2a
kyohei kabasawa <slifekb2@gmail.com>
2024-01-02 18:29:36 +0900

    tailwind cssが読み込まれない対応

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "nft_collections#index"
+  root 'nft_collections#index'
   resources :nft_collections, only: [:index, :show]
 
   get "up" => "rails/health#show", as: :rails_health_check
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ nft_collections = [
   {
     name: "Ponta A",
     contract_adress: "0xe6c1ee6624c6819262f29199dF25A70B2648B818",
-    image: "https://picsum.photos/300/200",
+    image: "https://i.seadn.io/gcs/files/272a5e53dbebab6cf3e6c332bdb79776.png?auto=format&dpr=1&w=1200",
     creater_name: "ponta ponta",
     creater_image: "https://placekitten.com/300/200",
     chain: "Sepolia"
